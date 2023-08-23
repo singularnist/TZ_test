f724a36b778741dadffc3e9414cb51dd095b08ee
singularnist <121468102+singularnist@users.noreply.github.com>
2023-08-24 02:51:04 +0300

    Update Delivery_db.py

--- db_work/models/Delivery_db.py
+++ db_work/models/Delivery_db.py
@@ -1,51 +1,57 @@
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import declarative_base, relationship, sessionmaker
-from sqlalchemy import Column, Integer, String, DECIMAL, DateTime
 from datetime import datetime
+
 import pymysql
+from sqlalchemy import  Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import declarative_base, relationship
+
+from .Produkt_db import Product
 
 pymysql.install_as_MySQLdb()
 Base = declarative_base()
+
 class Country(Base):
     __tablename__ = 'country'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    country = Column(String(255), nullable=False)
+    country = Column(String(100), nullable=False)
     def __repr__(self):
-        return '<country %r>' % self.id
+        return self.country
 
 class City(Base):
     __tablename__ = 'city'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    city = Column(String(255), nullable=False)
+    city = Column(String(100), nullable=False)
     def __repr__(self):
-        return '<city %r>' % self.id
+        return self.city
 
 class Street(Base):
     __tablename__ = 'street'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    street = Column(String(255), nullable=False)
+    street = Column(String(100), nullable=False)
     def __repr__(self):
-        return '<sity %r>' % self.id
+        return self.street
 
 class Delivery(Base):
     __tablename__ = 'delivery'
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
+    tovar_id = Column(Integer, ForeignKey(Product.id))
+    tovar = relationship(Product)
     quantity = Column(Integer, nullable=False)
-    country_id = Column(Integer, ForeignKey('country.id'), nullable=False)
-    city_id = Column(Integer, ForeignKey('city.id'), nullable=False)
-    street_id = Column(Integer, ForeignKey('street.id'), nullable=False)
+
+    country_id = Column(Integer, ForeignKey(Country.id))
+    country = relationship(Country)
+    city_id = Column(Integer, ForeignKey(City.id))
+    city = relationship(City)
+    street_id = Column(Integer, ForeignKey(Street.id))
+    street = relationship(Street)
+
     house = Column(String(10), nullable=False)
     number = Column(String(255), nullable=False)
     surname = Column(String(255), nullable=False)
     user_name = Column(String(255), nullable=False)
-    title = Column(String(255), nullable=False)
-    color = Column(String(255), nullable=False)
-    weight = Column(DECIMAL(10, 2), nullable=False)
-    price = Column(DECIMAL, nullable=False)
+    
     stat = Column(String(255), nullable=False)
     date = Column(DateTime, default=datetime.utcnow)
 
-    # Встановлюємо зв'язок між екземплярами класів, а не ID
-    country = relationship("Country", foreign_keys=[country_id])
-    city = relationship("City", foreign_keys=[city_id])
-    street = relationship("Street", foreign_keys=[street_id])
+
+
+
